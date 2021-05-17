99306c448ee309caae71163ca11c8b91fdb87c36
chaudharisucheta <chaudharisucheta@gmail.com>
2021-05-17 15:39:33 +0530

    XMLHttpRequest

--- Javascript/comments.js
+++ Javascript/comments.js
@@ -4,5 +4,49 @@ function getCommentData() {
   let personName = document.getElementById("personName").value;
   let comment = document.getElementById("content").value;
 
-  console.log(postId, email, personName, comment);
+  let commentPost = {
+    postId: postId,
+    email: email,
+    personName: personName,
+    comment: comment,
+  };
+  return commentPost;
+}
+
+function sendComment() {
+  let commentData = getCommentData();
+  console.log(commentData);
+  //1)Create object of XMLHttpRequest
+  //2)obj.open("method","url")
+  //3)obj.setRequestHeader("Content-type","application/json")
+  //4)obj.send(commentData) JSON.stringify(commentData)
+  //5)obj.onload, obj.onerror
+
+  let req = new XMLHttpRequest();
+  req.open("POST", "https://jsonplaceholder.typicode.com/comments");
+  req.setRequestHeader("Content-type", "application/json");
+  req.send(JSON.stringify(commentData));
+  req.onload = function () {
+    if (req.status == 201) {
+      console.log(req.response);
+      console.log("Submitted Successfully");
+    } else {
+      console.log(req.status);
+    }
+  };
+  req.onerror = function () {
+    console.log("Network Error!! Try again later");
+  };
+}
+
+function getAllComments() {
+  let commentReq = new XMLHttpRequest();
+  commentReq.open("GET", "https://jsonplaceholder.typicode.com/comments");
+  commentReq.setRequestHeader("Content-type", "application/json");
+  commentReq.send();
+  commentReq.onload = function () {
+    if (commentReq.status == 200) {
+      console.log(commentReq.response);
+    }
+  };
 }
