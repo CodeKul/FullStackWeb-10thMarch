78075ecb4521849896b8903f2e4cca6548da3582
chaudharisucheta <chaudharisucheta@gmail.com>
2021-05-21 15:35:21 +0530

    modules

---
+++ Javascript/myModule.js
@@ -0,0 +1,10 @@
+function myName() {
+  return "ABC";
+}
+function calculation(a, b) {
+  return a + b - 5;
+}
+
+let skills = ["HTML", "CSS", "Javascript"];
+let x = 500;
+export { myName, calculation, x ,skills}; //named exports
---
+++ Javascript/myModuleOne.js
@@ -0,0 +1,4 @@
+function person(sal, city, email) {
+  return [sal, city, email];
+}
+export default person ; //default export
