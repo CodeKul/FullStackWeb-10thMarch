ebfeb1164fae80703321948f701723644c915d91
chaudharisucheta <chaudharisucheta@gmail.com>
2021-06-01 15:32:59 +0530

    react-components

---
+++ ReactJS/basics/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponent from './Components-Class/MyComponent'
+import AnotherComponent from './Components-Class/AnotherComponent'
+function App() {
+  
+  return (
+    // jsx
+    <div>
+      <h1>Hello React</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea est pariatur reiciendis sit error, at temporibus numquam harum ipsum eveniet quaerat impedit officia iure aspernatur tenetur, in enim omnis commodi et! Error dolorum ut maiores, repellendus amet aut reiciendis odio commodi tempore molestiae quae cum magnam modi, harum nam aliquid?</p>
+      <MyComponent/>
+      <AnotherComponent/>
+    </div>
+  );
+}
+
+export default App;
