61324ce061d311a487a303329a9f5c94bd8bf48b
chaudharisucheta <chaudharisucheta@gmail.com>
2021-06-14 15:37:18 +0530

    useEffect

--- ReactJS/hooks/src/App.js
+++ ReactJS/hooks/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './useState/Counter';
+import Effect from './useEffect/Effect';
 function App() {
   return (
     <div>
         <h1>Hooks</h1>
-        <Counter/>
+        {/* <Counter/> */}
+        <Effect/>
     </div>
   );
 }
