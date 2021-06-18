9750a3706099ec6bf7116696a919f349bf0579e6
chaudharisucheta <chaudharisucheta@gmail.com>
2021-06-18 15:35:59 +0530

    forms

--- ReactJS/forms/src/forms/Registration.js
+++ ReactJS/forms/src/forms/Registration.js
@@ -10,19 +10,40 @@ function Registration() {
     message: "",
   });
 
+  const handleInputChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value,
+    });
+  };
+
+  const handleClick = () => {
+    console.log(user);
+  };
   return (
     // firstName,lastName,city-dropdown,gender,isActive,textarea
     <div style={{ marginLeft: "200px", marginTop: "80px" }}>
       <label htmlFor="firstname">FirstName</label>
-      <input type="text" />
+      <input
+        type="text"
+        name="firstName"
+        value={user.firstName}
+        onChange={handleInputChange}
+      />
       <br />
       <br />
       <label htmlFor="lastname">LastName</label>
-      <input type="text" />
+      <input
+        type="text"
+        name="lastName"
+        value={user.lastName}
+        onChange={handleInputChange}
+      />
       <br />
       <br />
       <label htmlFor="city">Select city </label>
-      <select name="" id="">
+      <select name="city" id="" value={user.city} onChange={handleInputChange}>
         <option value="pune">Pune</option>
         <option value="mumbai">Mumbai</option>
         <option value="thane">Thane</option>
@@ -33,20 +54,57 @@ function Registration() {
       <label htmlFor="gender">Select gender </label>
       <br />
       <br />
-      <input type="radio" />
+      <input
+        type="radio"
+        name="gender"
+        value="male"
+        checked={user.gender === "male"}
+        onChange={handleInputChange}
+      />
       <label htmlFor="male">Male</label>
-      <input type="radio" />
+      <input
+        type="radio"
+        name="gender"
+        value="female"
+        checked={user.gender === "female"}
+        onChange={handleInputChange}
+      />
       <label htmlFor="female">Female</label>
       <br />
       <br />
       <label htmlFor="isactive">Are you active?</label>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        name="isActive"
+        checked={user.isActive}
+        value={true}
+        onChange={handleInputChange}
+      />
       <br />
       <br />
       <label htmlFor="message">Leave a Message</label>
       <br />
       <br />
-      <textarea name="" id="message" cols="30" rows="5"></textarea>
+      <textarea
+        name="message"
+        value={user.message}
+        id="message"
+        cols="30"
+        rows="5"
+        onChange={handleInputChange}
+      ></textarea>
+
+      <button onClick={handleClick}>View</button>
+      <hr />
+      <h3>User Details</h3>
+      <ul>
+        <li>FirstName - {user.firstName}</li>
+        <li>LastName - {user.lastName}</li>
+        <li>City - {user.city}</li>
+        <li>Gender - {user.gender}</li>
+        <li>{user.isActive ? "Yes" : "No"}</li>
+        <li>Message - {user.message}</li>
+      </ul>
     </div>
   );
 }
