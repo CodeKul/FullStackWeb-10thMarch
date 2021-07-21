c9e42d77a73353a05571a07d4dddb1650b73c18d
chaudharisucheta <chaudharisucheta@gmail.com>
2021-07-21 18:02:37 +0530

    crud app

--- ReactJS/Books/booksui/src/App.js
+++ ReactJS/Books/booksui/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import BooksList from "./components/BooksList";
 import AddBook from "./components/AddBook";
+import EditBook from "./components/EditBook";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={BooksList}></Route>
           <Route path="/addbook" component={AddBook} />
+          <Route path="/editbook/:id" component={EditBook} />
         </Switch>
       </BrowserRouter>
     </div>
--- ReactJS/Books/booksui/src/components/BooksList.js
+++ ReactJS/Books/booksui/src/components/BooksList.js
@@ -14,6 +14,11 @@ function BooksList() {
     setBooks(response.data);
     console.log(booksList);
   };
+
+  const deleteBook = async (id) => {
+    await apicall.delete(`/books/${id}`);
+    getBooksList();
+  };
   return (
     <div className="container mt-5">
       <div className="text-end mb-3 mt-5">
@@ -29,6 +34,7 @@ function BooksList() {
             <th>Book Name</th>
             <th>Author Name</th>
             <th>Rating</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -38,6 +44,29 @@ function BooksList() {
               <td>{book.bookName}</td>
               <td>{book.authorName}</td>
               <td>{book.rating}</td>
+              <td className="d-flex justify-content-around">
+                <Link
+                  to={`/editbook/${book.id}`}
+                  //className="btn btn-outline-light"
+                >
+                  <i
+                    className="far fa-edit"
+                    style={{ color: "white", fontSize: "20px" }}
+                  ></i>
+                </Link>
+                <button
+                  className="btn btn-dark mr-0"
+                  onClick={() => deleteBook(book.id)}
+                >
+                  <i
+                    className="far fa-trash-alt"
+                    style={{
+                      color: "white",
+                      fontSize: "20px",
+                    }}
+                  ></i>
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
