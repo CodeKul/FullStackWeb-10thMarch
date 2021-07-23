7c2ba03dc51c2474e69a0226d0768adf286646dc
chaudharisucheta <chaudharisucheta@gmail.com>
2021-07-23 18:09:23 +0530

    landing page

--- ReactJS/landingpage/src/Home.js
+++ ReactJS/landingpage/src/Home.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import sea from "./sea.mp4";
 import "./Home.css";
 function Home() {
+  const [isOpen, setOpen] = useState(false);
   return (
     <div className="main">
       <header>
         <h2 className="logo-text">Travel</h2>
 
-        <i className="fas fa-bars menu-icon"></i>
+        <i
+          className={`menu-icon ${
+            isOpen ? "fas fa-times move" : "fas fa-bars"
+          }`}
+          onClick={() => setOpen(!isOpen)}
+        ></i>
       </header>
       <div className="text">
         <h2 className="heading-text">Adventure Awaits</h2>
@@ -18,18 +24,27 @@ function Home() {
         </p>
         <button className="btn-explore">Explore</button>
       </div>
-      <video src={sea} controls></video>
+      <video src={sea} controls={false} autoPlay={true} muted></video>
       <ul className="social-icons">
         <li>
           <i className="fab fa-facebook-f"></i>
         </li>
         <li>
-          <i class="fab fa-instagram"></i>
+          <i className="fab fa-instagram"></i>
         </li>
         <li>
-          <i class="fab fa-twitter"></i>
+          <i className="fab fa-twitter"></i>
         </li>
       </ul>
+      <div className={`menu ${isOpen ? "active" : ""}`}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Review</li>
+          <li>Plan Travel</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
   );
 }
